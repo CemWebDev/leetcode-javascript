734d243e4b83c1f4a290714368e1a3a2df63ef7d
Cem <bcemcankaya@gmail.com>
2024-09-04 20:11:58 +0300

    formatting javascript file

--- easy/array-reduce-transformation.js
+++ easy/array-reduce-transformation.js
@@ -1,7 +1,6 @@
 const reduce = (nums, fn, init) => {
-    nums.forEach((element) => {
-        init = fn(init, element);
-
-    });
-    return init;
+  nums.forEach((element) => {
+    init = fn(init, element);
+  });
+  return init;
 };
