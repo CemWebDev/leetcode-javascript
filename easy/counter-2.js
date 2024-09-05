b921fe4fd6548e4c055778bd832e8b428d657e12
Cem <bcemcankaya@gmail.com>
2024-09-05 21:00:49 +0300

    counter-ii description

--- easy/counter-2.js
+++ easy/counter-2.js
@@ -1,3 +1,13 @@
+/**
+ * Creates a counter object with methods to increment, decrement, and reset the counter.
+ *
+ * @param {number} init - The initial value of the counter.
+ * @returns {object} - An object with three methods:
+ *   - `increment()`: Increases the counter by 1 and returns the new value.
+ *   - `reset()`: Resets the counter to the initial value and returns the new value.
+ *   - `decrement()`: Decreases the counter by 1 and returns the new value.
+ */
+
 const createCounter = (init) => {
   let counter = init;
   return {
