8ab1f0e6be83d3074db4410945791699d2710733
Cem <bcemcankaya@gmail.com>
2024-09-08 16:37:21 +0300

    memoize task solution

--- medium/memoize.js
+++ medium/memoize.js
@@ -2,9 +2,18 @@
  * @param {Function} fn
  * @return {Function}
  */
-function memoize(fn) {
-    
-    return function(...args) {
-        
+
+const memoize = (fn) => {
+  const cache = new Map();
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) {
+      return cache.get(key);
     }
-}
+    const result = fn(...args);
+    cache.set(key, result);
+
+    return result;
+  };
+};
